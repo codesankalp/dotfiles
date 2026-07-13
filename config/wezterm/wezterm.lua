85836133609eeb35e0a1ab260d2707f8e34a1cfc
Sankalp <sankalp.nex@gmail.com>
2026-07-13 11:07:19 +0530

    chore: polish wezterm/tmux, swap asdf for mise, tidy zsh env
    - wezterm: native macOS fullscreen, zsh-friendly OPT/CMD keybindings,
      CMD+click link opening that bypasses tmux mouse reporting
    - tmux: slim status line (session + cwd left; flags, time, host right)
    - zsh: rely on brew shellenv instead of hand-rolled PATH/GOROOT,
      hook direnv, add grok installer block, kexec fzf pod picker, c alias
    - Brewfile: replace asdf with mise

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,8 +2,8 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.initial_cols = 140
-config.initial_rows = 40
+config.initial_cols = 120
+config.initial_rows = 60
 
 config.color_scheme = 'rose-pine-moon'
 config.max_fps = 120
@@ -28,6 +28,7 @@ config.window_background_opacity = 0.8
 config.macos_window_background_blur = 50
 config.font_size = 15.0
 config.window_frame.font_size = 13.0
+config.native_macos_fullscreen_mode = true
 
 config.keys = {
   {
@@ -44,6 +45,68 @@ config.keys = {
     key = 'Enter',
     mods = 'CMD',
     action = wezterm.action.ToggleFullScreen,
+  },
+  {
+    key = 'f',
+    mods = 'CMD|CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  },
+  -- zsh's emacs keymap moves by word on ESC-b/ESC-f, not OPT+Arrow
+  {
+    key = 'LeftArrow',
+    mods = 'OPT',
+    action = wezterm.action.SendKey { key = 'b', mods = 'ALT' },
+  },
+  {
+    key = 'RightArrow',
+    mods = 'OPT',
+    action = wezterm.action.SendKey { key = 'f', mods = 'ALT' },
+  },
+  -- zsh binds backward-kill-word to Ctrl-W
+  {
+    key = 'Backspace',
+    mods = 'OPT',
+    action = wezterm.action.SendString '\x17',
+  },
+  -- zsh binds beginning/end-of-line to Ctrl-A/Ctrl-E
+  {
+    key = 'LeftArrow',
+    mods = 'CMD',
+    action = wezterm.action.SendString '\x01',
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CMD',
+    action = wezterm.action.SendString '\x05',
+  },
+  -- zsh binds kill-whole-line to Ctrl-U
+  {
+    key = 'Backspace',
+    mods = 'CMD',
+    action = wezterm.action.SendString '\x15',
+  },
+  {
+    key = 'k',
+    mods = 'CMD',
+    action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+  },
+}
+
+-- tmux's `mouse on` captures clicks before wezterm's link handling sees them;
+-- holding CMD bypasses that (wezterm's default bypass modifier is SHIFT)
+config.bypass_mouse_reporting_modifiers = 'SUPER'
+
+config.mouse_bindings = {
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'SUPER',
+    action = wezterm.action.OpenLinkAtMouseCursor,
+  },
+  -- swallow the paired Down event so the click isn't also sent to the pane
+  {
+    event = { Down = { streak = 1, button = 'Left' } },
+    mods = 'SUPER',
+    action = wezterm.action.Nop,
   },
 }
 
